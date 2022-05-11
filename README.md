2f04be51ed98560a5ff21fc32eb8f2150d04e4a6
Purwandi <free6300@gmail.com>
2022-05-11 22:13:10 +0700

    update sample

---
+++ README.md
@@ -0,0 +1,1 @@
+https://www.jaegertracing.io/docs/1.19/client-libraries/#emsgsize-and-udp-buffer-limits
--- user-python/app/bootstrap/tracer.py
+++ user-python/app/bootstrap/tracer.py
@@ -4,6 +4,7 @@ from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.sampling import TraceIdRatioBased
 
 # from opentelemetry.instrumentation.asyncpg import AsyncPGInstrumentor
 
@@ -12,13 +13,19 @@ def tracer_create(name: str, agent: str, collector: str):
     if agent == "":
         agent = "localhost:6831"
 
+    sampler = TraceIdRatioBased(1)  # 1/10
     resource = Resource.create({SERVICE_NAME: name})
-    trace.set_tracer_provider(TracerProvider(resource=resource))
+    trace.set_tracer_provider(TracerProvider(resource=resource, sampler=sampler))
     tracer = trace.get_tracer(__name__)
+
+    # di POC kita menggunakan http thrift, karena issue
+    # https://www.jaegertracing.io/docs/1.19/client-libraries/#emsgsize-and-udp-buffer-limits
 
     hostname, port = agent.split(":")
     exporter = JaegerExporter(
-        agent_host_name=hostname, agent_port=int(port), collector_endpoint=collector
+        # agent_host_name=hostname,
+        # agent_port=int(port),
+        collector_endpoint=collector
     )
     trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(exporter))  # type: ignore
 
